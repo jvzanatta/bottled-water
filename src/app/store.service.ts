12a8c48689cd05b5ef511082ca5630ca933dfcfc
João Victor Zanatta <joaovictor15@gmail.com>
2018-08-15 23:33:01 -0300

    Adding classes and url to the service

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+import { environment } from '@env/environment';
+
+const API_URL = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  public getStore(): Observable<Todo[]> {
+    return this.http
+      .get(API_URL + '/todos')
+      .map(response => {
+        const todos = response.json();
+        return todos.map((todo) => new Todo(todo));
+      })
+      .catch(this.handleError);
+  }
+
 }
