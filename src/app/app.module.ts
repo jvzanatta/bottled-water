77c53349375e662cb23563adffe0f854148fcfb6
João Victor Zanatta <joaovictor15@gmail.com>
2018-08-16 00:14:32 -0300

    Starting home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProductComponent } from './product/product.component';
 import { BannerComponent } from './banner/banner.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +19,12 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     TopBarComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
   ],
-  providers: [],
+  providers: [
+    HttpModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
