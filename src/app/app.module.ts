bbb5e9f1275893c887db9bb06a5de75420f8bcb4
João Victor Zanatta <joaovictor15@gmail.com>
2018-08-15 21:28:32 -0300

    Generating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProductComponent } from './product/product.component';
+import { BannerComponent } from './banner/banner.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProductComponent,
+    BannerComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
